64c6bf91c8322cc5e0841bd039aee38d6ab7d708
sunny-zerror <sunny.zerror@gmail.com>
2025-09-24 12:36:30 +0530

    dummy code

--- src/components/Home/ServiceSlider.jsx
+++ src/components/Home/ServiceSlider.jsx
@@ -46,23 +46,14 @@ const ServiceSlider = () => {
     return (
         <div>
             <div className=" serv_slider_paren w-full center mt-20 text-[#FFFDF6] h-screen relative">
-                <img className=' serv_slide_bg_img_1 w-full h-full absolute object-cover top-0 left-0 z-[4] ' src="/images/HomePage/serviceSlider/serviceImg1.webp" alt="" />
+                <img className=' serv_slide_bg_img_1 w-full h-full absolute object-cover top-0 left-0 z-[4] ' src="/Images/HomePage/FooterImg.webp" alt="" />
                 <img className=' serv_slide_bg_img_2 w-full h-full absolute object-cover top-0 left-0 z-[3] ' src="/images/HomePage/serviceSlider/serviceImg2.webp" alt="" />
                 <img className=' serv_slide_bg_img_3 w-full h-full absolute object-cover top-0 left-0 z-[2] ' src="/images/HomePage/serviceSlider/serviceImg3.webp" alt="" />
                 <img className=' serv_slide_bg_img_4 w-full h-full absolute object-cover top-0 left-0 z-[1] ' src="/images/HomePage/serviceSlider/serviceImg4.webp" alt="" />
 
                 <div className=" serv_slide_bg_img_1 absolute w-[90%] h-[85%] bg-[#454738] z-[15] p-8">
                     <div className=" w-full h-[40%]  pb-5 flex flex-col justify-between">
-                        <div className=" flex  justify-between">
-                            <div className="flex gap-2 uppercase text-4xl">
-                                <p>Our</p>
-                                <h2 className='italic'>Services</h2>
-                            </div>
-                            <div className="flex gap-2 items-end">
-                                <p className='text-7xl'>01/</p>
-                                <p className='text-5xl opacity-50'>04</p>
-                            </div>
-                        </div>
+                        
                         <div className=" flex items-end justify-between">
                             <p className='text-5xl uppercase'>Residential Interior </p>
                             <div className="w-[30%] leading-none">
@@ -71,7 +62,7 @@ const ServiceSlider = () => {
                         </div>
                     </div>
                     <div className="h-[60%] w-full">
-                        <img className='w-full h-full object-cover ' src="/images/HomePage/serviceSlider/serviceImg1.webp" alt="" />
+                        <img className='w-full h-full object-cover ' src="/Images/HomePage/FooterImg.webp" alt="" />
                     </div>
                 </div>
 
